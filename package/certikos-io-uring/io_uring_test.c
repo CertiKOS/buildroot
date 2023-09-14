4a2b0e0975a10eb0972ef49a8d687d4b8325c137
Richard Habeeb <richard.habeeb@gmail.com>
2023-09-14 16:45:25 -0400

    minor revisions to proxy

--- package/certikos-io-uring/io_uring_test.c
+++ package/certikos-io-uring/io_uring_test.c
@@ -36,151 +36,6 @@ int io_uring_enter(int ring_fd, unsigned int to_submit,
 }
 
 
-///*
-//* Read from completion queue.
-//* In this function, we read completion events from the completion queue.
-//* We dequeue the CQE, update and head and return the result of the operation.
-//* */
-//int read_from_cq() {
-//    struct io_uring_cqe *cqe;
-//    unsigned head;
-//
-//    /* Read barrier */
-//    head = io_uring_smp_load_acquire(cring_head);
-//
-//
-//    /*
-//    * Remember, this is a ring buffer. If head == tail, it means that the
-//    * buffer is empty.
-//    * */
-//    if (head == *cring_tail)
-//        return -1;
-//
-//    /* Get the entry */
-//    cqe = &cqes[head & (*cring_mask)];
-//
-//    if (cqe->res < 0)
-//        fprintf(stderr, "Error: %s\n", strerror(abs(cqe->res)));
-//
-//    head++;
-//
-//
-//    /* Write barrier so that update to the head are made visible */
-//    io_uring_smp_store_release(cring_head, head);
-//
-//    return cqe->res;
-//}
-//
-//
-//
-///*
-//* Submit a read or a write request to the submission queue.
-//*
-//* */
-//int submit_to_sq(int fd, int op) {
-//    unsigned index, tail;
-//
-//    /* Add our submission queue entry to the tail of the SQE ring buffer */
-//    tail = *sring_tail;
-//
-//    index = tail & *sring_mask;
-//
-//    struct io_uring_sqe *sqe = &sqes[index];
-//
-//    /* Fill in the parameters required for the read or write operation */
-//    sqe->opcode = op;
-//
-//
-//    sqe->fd = fd;
-//    sqe->addr = (unsigned long) buff;
-//
-//
-//    if (op == IORING_OP_READ) {
-//        memset(buff, 0, sizeof(buff));
-//        sqe->len = BLOCK_SZ;
-//    }
-//    else {
-//        sqe->len = strlen(buff);
-//    }
-//
-//    sqe->off = offset;
-//
-//    sring_array[index] = index;
-//
-//    tail++;
-//
-//
-//    /* Update the tail */
-//    io_uring_smp_store_release(sring_tail, tail);
-//
-//
-//    /*
-//    * Tell the kernel we have submitted events with the io_uring_enter()
-//    * system call. We also pass in the IOURING_ENTER_GETEVENTS flag which
-//    * causes the io_uring_enter() call to wait until min_complete
-//    * (the 3rd param) events complete.
-//    * */
-//    int ret =  io_uring_enter(ring_fd, 1,1,
-//                              IORING_ENTER_GETEVENTS);
-//
-//    if(ret < 0) {
-//        perror("io_uring_enter");
-//        return -1;
-//    }
-//
-//    return ret;
-//}
-
-
-
-
-//int main(int argc, char *argv[]) {
-//    int res;
-//
-//
-//    /* Setup io_uring for use */
-//    if(app_setup_uring()) {
-//        fprintf(stderr, "Unable to setup uring!\n");
-//        return 1;
-//    }
-//
-//
-//    /*
-//    * A while loop that reads from stdin and writes to stdout.
-//    * Breaks on EOF.
-//    */
-//    while (1) {
-//        /* Initiate read from stdin and wait for it to complete */
-//        submit_to_sq(STDIN_FILENO, IORING_OP_READ);
-//
-//        /* Read completion queue entry */
-//        res = read_from_cq();
-//
-//        if (res > 0) {
-//
-//            /* Read successful. Write to stdout. */
-//            submit_to_sq(STDOUT_FILENO, IORING_OP_WRITE);
-//
-//            read_from_cq();
-//
-//        } else if (res == 0) {
-//            /* reached EOF */
-//            break;
-//        }
-//        else if (res < 0) {
-//            /* Error reading file */
-//            fprintf(stderr, "Error: %s\n", strerror(abs(res)));
-//            break;
-//        }
-//
-//        offset += res;
-//    }
-//
-//    return 0;
-//}
-
-
-
 
 int main(int argc, char *argv[])
 {
@@ -190,7 +45,8 @@ int main(int argc, char *argv[])
     p.sq_thread_idle = 120000; /* 2 minutes timeout */
     p.resv[0] = 9; /* enclave id */
 
-    printf("io_uring_setup for enclave id=%u...\n", p.resv[0]);
+    printf("io_uring proxy: io_uring_setup for enclave id=%u...\n", p.resv[0]);
+
     long ret = io_uring_setup(16, &p);
 
     void* rings = mmap(0, p.sq_off.array + p.sq_entries * sizeof(uint32_t),
@@ -203,20 +59,20 @@ int main(int argc, char *argv[])
     (void)sqes;
 
     void * sq_ptr = rings;
-    void * cq_ptr = rings;
-    uint32_t *sring_head = sq_ptr + p.sq_off.tail;
-    uint32_t *sring_tail = sq_ptr + p.sq_off.tail;
+    //void * cq_ptr = rings;
+    //uint32_t *sring_head = sq_ptr + p.sq_off.tail;
+    //uint32_t *sring_tail = sq_ptr + p.sq_off.tail;
     uint32_t *sring_flags = sq_ptr + p.sq_off.flags;
 
-    uint32_t *cring_head = cq_ptr + p.cq_off.head;
-    uint32_t *cring_tail = cq_ptr + p.cq_off.tail;
+    //uint32_t *cring_head = cq_ptr + p.cq_off.head;
+    //uint32_t *cring_tail = cq_ptr + p.cq_off.tail;
     //uint32_t *sring_mask = sq_ptr + p.sq_off.mask;
     //uint32_t *sring_array = sq_ptr + p.sq_off.array;
 
     while(1) {
         if(io_uring_smp_load_acquire(sring_flags) & IORING_SQ_NEED_WAKEUP)
         {
-            printf("Waking up sleeping sq thread\n");
+            printf("io_uring proxy: waking up sleeping sq thread\n");
             io_uring_enter(ret, 0, 0, IORING_ENTER_SQ_WAKEUP);
         }
         sleep(1);
