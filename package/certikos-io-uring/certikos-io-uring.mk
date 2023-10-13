45e3cb36131ef03ee9dafa30ea5a8d03324de2a7
Richard Habeeb <richard.habeeb@gmail.com>
2023-10-13 15:37:05 -0400

    rename io_uring_text to ringleader_proxy, add periodic test application

--- package/certikos-io-uring/certikos-io-uring.mk
+++ package/certikos-io-uring/certikos-io-uring.mk
@@ -7,15 +7,18 @@
 CERTIKOS_IO_URING_LICENSE = Public Domain
 
 define CERTIKOS_IO_URING_EXTRACT_CMDS
-	cp package/certikos-io-uring/io_uring_test.c $(@D)/
+	cp package/certikos-io-uring/ringleader_proxy.c $(@D)/
+	cp package/certikos-io-uring/periodic_ping.c $(@D)/
 endef
 
 define CERTIKOS_IO_URING_BUILD_CMDS
-	(cd $(@D); $(TARGET_CC) -Wall -Os -s io_uring_test.c -o io_uring_test)
+	(cd $(@D); $(TARGET_CC) -Wall -Os -s ringleader_proxy.c -o ringleader_proxy)
+	(cd $(@D); $(TARGET_CC) -Wall -Os -s periodic_ping.c -o periodic_ping)
 endef
 
 define CERTIKOS_IO_URING_INSTALL_TARGET_CMDS
-	install -m 0755 -D $(@D)/io_uring_test $(TARGET_DIR)/usr/bin/io_uring_test
+	install -m 0755 -D $(@D)/ringleader_proxy $(TARGET_DIR)/usr/bin/ringleader_proxy
+	install -m 0755 -D $(@D)/periodic_ping $(TARGET_DIR)/usr/bin/periodic_ping
 endef
 
 $(eval $(generic-package))
