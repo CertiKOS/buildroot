d1cfe557c227e0f629204e3d8df91f3b08acc7d5
Richard Habeeb <richard.habeeb@gmail.com>
2023-10-24 23:39:24 -0400

    Organizing packages for CertiKOS

---
+++ package/certikos/io-uring-perf/io-uring-perf.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# io-uring-perf
+#
+################################################################################
+
+IO_URING_PERF_LICENSE = Public Domain
+
+define IO_URING_PERF_EXTRACT_CMDS
+	cp package/certikos/io-uring-perf/io_uring_echo.c $(@D)/
+endef
+
+define IO_URING_PERF_BUILD_CMDS
+	(cd $(@D); $(TARGET_CC) -Wall -Os -s io_uring_echo.c -o io_uring_echo)
+endef
+
+define IO_URING_PERF_INSTALL_TARGET_CMDS
+	install -m 0755 -D $(@D)/io_uring_echo $(TARGET_DIR)/usr/bin/io_uring_echo
+endef
+
+$(eval $(generic-package))
